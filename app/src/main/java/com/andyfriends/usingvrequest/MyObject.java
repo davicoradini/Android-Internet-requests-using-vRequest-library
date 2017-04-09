8f9121991dd89332ab9dace10bc9b6c029d6de85
Davi Daniel <davicoradini@gmail.com>
2017-04-08 23:11:47 -0300

    First commit

---
+++ app/src/main/java/com/andyfriends/usingvrequest/MyObject.java
@@ -0,0 +1,16 @@
+package com.andyfriends.usingvrequest;
+
+/**
+ * Created by davic on 2017-04-05.
+ */
+
+public class MyObject {
+
+    private String one;
+    private String key;
+
+    @Override
+    public String toString() {
+        return "one = " + one + " / key = " + key;
+    }
+}
