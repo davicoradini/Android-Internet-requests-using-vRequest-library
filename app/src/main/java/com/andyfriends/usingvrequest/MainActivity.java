767293be5fa1ab765bf374c10e87bb9079ab233c
Davi Daniel <davicoradini@gmail.com>
2017-04-09 22:46:01 -0300

    Tests

--- app/src/main/java/com/andyfriends/usingvrequest/MainActivity.java
+++ app/src/main/java/com/andyfriends/usingvrequest/MainActivity.java
@@ -5,7 +5,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
 import com.android.volley.Response;
-import com.android.volley.VolleyError;
 import com.andyfriends.vrequest.VRequest;
 
 public class MainActivity extends AppCompatActivity {
@@ -25,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
     private void fetchSomething() {
         status.setText("SENDING REQUEST...");
 
-        new VRequest()
+        /*new VRequest()
                 .with(this)
                 .load("http://echo.jsontest.com/key/value/one/two")
                 .onSuccess(new Response.Listener<MyObject>() {
@@ -43,7 +42,24 @@ public class MainActivity extends AppCompatActivity {
                 .onError(new Response.ErrorListener() {
                     @Override
                     public void onErrorResponse(VolleyError error) {
+                        //we don't need to set both onError
+                    }
+                })
+                .fetch();*/
 
+        new VRequest()
+                .with(this)
+                .load(new FlaskRequestLoadConf("http://echo.jsontest.com/key/value/one/two"))
+                .onSuccess(new Response.Listener<MyObject>() {
+                    @Override
+                    public void onResponse(MyObject myObject) {
+                        status.setText("Response OK: " + myObject.toString());
+                    }
+                })
+                .onError(new Response.Listener() {
+                    @Override
+                    public void onResponse(Object response) {
+                        status.setText("error - check Internet permission on Manifest");
                     }
                 })
                 .fetch();
